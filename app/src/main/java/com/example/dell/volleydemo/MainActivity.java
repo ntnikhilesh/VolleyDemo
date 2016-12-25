596c526ee5807886a2eb60834e3423be553cf5df
nikhil <ntnikhilesh@gmail.com>
2016-12-25 15:48:03 +0530

    get string resopnse from Server using Volley

---
+++ app/src/main/java/com/example/dell/volleydemo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.dell.volleydemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    TextView server_response;
+    Button get_response;
+    String server_url="http://172.16.0.2/greetings.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        server_response=(TextView)findViewById(R.id.tv_server_response);
+        get_response=(Button)findViewById(R.id.button_get_resp);
+
+        get_response.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create request queue
+                final RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+
+                //Create String Request(1st argument -Method type,2-URL,3, Response Listner , Error Listner
+                StringRequest stringRequest=new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                server_response.setText(response);
+                                //stop request queue
+                                requestQueue.stop();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        server_response.setText("Something went wrong...");
+                        error.printStackTrace();
+                        requestQueue.stop();
+
+                    }
+                });
+
+                //Add request to Request Queue
+                requestQueue.add(stringRequest);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
